0f9f76e48b497529d070f7eb440ba620440127de
contattiflowstudio-web <contattiflowstudio@gmail.com>
2026-05-18 23:13:32 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "@/i18n/LanguageContext";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import ServicesPage from "./pages/Services.tsx";
+import WhyDigitalPage from "./pages/WhyDigital.tsx";
+import PortfolioPage from "./pages/Portfolio.tsx";
+import ContactPage from "./pages/Contact.tsx";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -15,9 +20,14 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/flow-studio-ignition/">
+        <BrowserRouter basename="/">
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/services" element={<ServicesPage />} />
+            <Route path="/why-digital" element={<WhyDigitalPage />} />
+            <Route path="/portfolio" element={<PortfolioPage />} />
+            <Route path="/contact" element={<ContactPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
