264ffb6722c88395ceb0daf7e85503cb693c98e2
contattiflowstudio-web <contattiflowstudio@gmail.com>
2026-05-18 23:26:50 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/flowstudio/">
+        <BrowserRouter basename=>
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
